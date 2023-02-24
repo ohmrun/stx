574f865712cfe3e322f78aa4c748b2f2f0a44a71
0b1kn00b <polysemantic@gmail.com>
2023-02-24 10:05:05 +0100

    Initial Commit

---
+++ load.py
@@ -0,0 +1,11 @@
+import os.path
+import fs
+import json
+import shutil
+
+str = open("sources.json","r")
+jsn = json.load(str)
+
+for prj in jsn["directories"]:
+    shutil.copytree(f'../{prj}/src/main/haxe/','src/main/haxe/',dirs_exist_ok=True)
+
