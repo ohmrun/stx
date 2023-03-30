7aeee5e26f8f5ac26e4e007268624a4fed1a4cfd
0b1kn00b <polysemantic@gmail.com>
2023-03-30 07:33:26 +0200

    remove source files before refresh

---
+++ README.md
@@ -0,0 +1,10 @@
+# stx_release
+
+This is the build repo for the stx library, containing all the published ohmrun libraries fit to print.
+
+## !git url subject to change.
+
+### Install 
+```
+  haxelib git stx https://github.com/ohmrun/stx_release 
+```
--- load.py
+++ load.py
@@ -2,9 +2,11 @@ import os.path
 import fs
 import json
 import shutil
-
+import os
 str = open("sources.json","r")
 jsn = json.load(str)
+
+os.system('rm -rf src/main/haxe')
 
 for prj in jsn["directories"]:
     shutil.copytree(f'lib/{prj}/src/main/haxe/','src/main/haxe/',dirs_exist_ok=True)
